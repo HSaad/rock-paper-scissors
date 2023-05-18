0e5ad11745c15604b5ee576a3a6a5b850e68d964
Hagr Saad <hagrsaad16@gmail.com>
2023-05-18 13:01:02 -0400

    Add game function and getPlayerSelection function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function random(){
     return Math.floor(Math.random() * 3);
 }
@@ -14,29 +17,34 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase().trim();
-
+    
     switch (playerSelection){
         case "rock":
             if(computerSelection == "rock"){
                 return "Tie!";
             } else if (computerSelection == "paper"){
+                computerScore++;
                 return "You Lose! Paper beats Rock";
             }else {
+                playerScore++;
                 return "You Win! Rock beats Scissors";
             }
         case "paper":
             if (computerSelection == "rock"){
+                playerScore++;
                 return "You Win! Paper beats Rock";
             } else if (computerSelection == "paper"){
                 return "Tie!";
             } else {
+                computerScore++;
                 return "You Lose! Scissors beats Paper";
             }
         case "scissors":
             if (computerSelection == "rock"){
+                computerScore++;
                 return "You Lose! Rock beats Scissors";
             }else if (computerSelection == "paper"){
+                playerScore++;
                 return "You Win! Scissors beats Paper";
             }else {
                 return "Tie!";
@@ -46,6 +54,31 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function getWinner(){
+    if(computerScore > playerScore){
+        return `Computer Wins. Computer Score: ${computerScore},  Your Score: ${playerScore}`;
+    }else if(computerScore < playerScore ){
+        return `You Win! Computer Score: ${computerScore},  Your Score: ${playerScore}`;
+    }else {
+        return `Tie! Computer Score: ${computerScore},  Your Score: ${playerScore}`;
+    }
+}
+
+function getPlayerChoice(){
+    let playerSelection = prompt("Choose: rock, paper, or scissors").toLowerCase().trim();
+
+    while(playerSelection != "rock" && playerSelection != "scissors" && playerSelection != "paper"){
+        playerSelection = prompt("Incorrect Selection. Please choose either: rock, paper, or scissors").toLowerCase().trim();
+    }
+
+    return playerSelection;
+}
+
+function game() {
+    for(let i =0; i < 5; i++){
+        console.log(playRound(getPlayerChoice(), getComputerChoice()));
+    }
+    console.log(getWinner());
+}
+
+game();
